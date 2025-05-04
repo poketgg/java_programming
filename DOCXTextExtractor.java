c21cfa89d398759c7a0b2e8ba47d5a470b551712
qudgusska <otdootpo@naver.com>
2025-05-05 04:37:36 +0900

    Add files via upload

--- DOCXTextExtractor.java
+++ DOCXTextExtractor.java
@@ -1,5 +1,27 @@
 package com.example;
 
-public class DOCXTextExtractor {
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
+import java.util.List;
+
+public class DOCXTextExtractor {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("파일이름.docx"); // 같은 폴더에 넣을 것
+            XWPFDocument document = new XWPFDocument(fis);
+            List<XWPFParagraph> paragraphs = document.getParagraphs();
+
+            for (XWPFParagraph para : paragraphs) {
+                System.out.println(para.getText());
+            }
+
+            document.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- XLSXTextExtractor.java
+++ XLSXTextExtractor.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 public class XLSXTextExtractor {
     public static void main(String[] args) {
         try {
-            FileInputStream file = new FileInputStream("sample.xlsx"); // 같은 폴더에 넣을 것
+            FileInputStream file = new FileInputStream("파일이름.xlsx"); // 같은 폴더에 넣을 것
             XSSFWorkbook workbook = new XSSFWorkbook(file);
             XSSFSheet sheet = workbook.getSheetAt(0);
 
