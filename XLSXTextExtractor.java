d61b327ecc25c9b86193f3f0648b563574534258
qudgusska <otdootpo@naver.com>
2025-05-05 04:34:52 +0900

    Add files via upload

---
+++ XLSXTextExtractor.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.apache.poi.xssf.usermodel.*;
+import java.io.FileInputStream;
+
+public class XLSXTextExtractor {
+    public static void main(String[] args) {
+        try {
+            FileInputStream file = new FileInputStream("sample.xlsx"); // 같은 폴더에 넣을 것
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+                XSSFRow row = sheet.getRow(i);
+                for (int j = 0; j < row.getPhysicalNumberOfCells(); j++) {
+                    XSSFCell cell = row.getCell(j);
+                    System.out.print(cell.toString() + "\t");
+                }
+                System.out.println();
+            }
+
+            workbook.close();
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
